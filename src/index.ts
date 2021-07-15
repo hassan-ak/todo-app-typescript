3f8a86ba684a4874349776eca881d67c8957c364
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-07-15 19:12:46 +0500

    Step04:- Test Data Models
    1:- Updated
    	a:- src/index.ts
    2:- Compiled and Executed

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,50 @@
+// Imports
 import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
 
-let todoItem : TodoItem = new TodoItem(1,"Hello World")
+// List of Todos
+let todos : TodoItem[] = [
+    new TodoItem(1, "Go for run", true),
+    new TodoItem(2, "Play Cricket", false),
+    new TodoItem(5, "Go to Class"), 
+    new TodoItem(6, "Eat Lunch",true)
+];
 
-todoItem.printDetails()
+// Collection of todos
+let collection : TodoCollection = new TodoCollection("Hassan", todos);
+
+// Clear console
+console.clear();
+
+// Print Collection (Pre defined)
+console.log("*** Original Collection ***")
+console.log(`${collection.userName}'s Todo List`);
+collection.printAll()
+
+// add new todo to the collection
+// this will return a id of the newly added todo
+let newId1 = collection.addTodo("Watch Movie");
+let newId2 = collection.addTodo("Go to Sleep");
+
+// get todo's by Id
+let todoItem1 = collection.getTodoById(newId1);
+let todoItem2 = collection.getTodoById(newId2);
+
+// Print details of todo items fetched by id
+console.log("\n*** Todo's fetched by ID ***")
+todoItem1.printDetails();
+todoItem2.printDetails();
+
+// Print Updated Collection
+console.log("\n*** Updated Collection with new todo's ***")
+console.log(`${collection.userName}'s Todo List`);
+collection.printAll()
+
+// Update complete status of todos
+collection.markComplete(2,true)
+collection.markComplete(6,false)
+
+// Print Updated Collection
+console.log("\n*** Updated Collection with Complete Status Changed ***")
+console.log(`${collection.userName}'s Todo List`);
+collection.printAll()
